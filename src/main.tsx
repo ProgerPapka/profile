5997c5a70301865c74b06ae97d0fed3ce234f6b9
e.ziyatdinov <e.ziyatdinov@tinkoff.ru>
2023-02-18 20:13:41 +0300

    feat: added default loader

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import {render} from 'preact';
 import {App} from './app';
 import './index.scss';
 
+const rootLoader = document.getElementById('root-loader');
+rootLoader?.remove();
+
 render(<App />, document.getElementById('app') as HTMLElement);
