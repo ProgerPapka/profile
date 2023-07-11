3ca21ea958179eef74fd3bf392961c5f712bf963
Eldar Ziiatdinov <ziatdinov.eldar9@gmail.com>
2023-07-12 03:50:14 +0400

    feat(styles): added grids and containers

--- src/app.tsx
+++ src/app.tsx
@@ -7,16 +7,20 @@ export function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <img src="/profile.png" class="logo" alt="My photo" />
+    <div class="container container_adaptive">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 center">
+          <img src="/profile.png" class="logo" alt="My photo" />
+        </div>
       </div>
-      <div class="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 center">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+        </div>
       </div>
-      <Loader />
-    </>
+    </div>
   );
 }
